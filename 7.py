10b9f9ee8e49a711590a68878c087ca5bccf21da
bKamilla <k.batrakova@g.nsu.ru>
2024-02-16 23:59:00 +0700

    7.1

--- 7.py
+++ 7.py
@@ -1,3 +1,9 @@
 # Practicum-1
 # Developer: Batrakova K.
 #
+print('Как Вас зовут?')
+name = input()
+print('Здравствуйте, ',name)
+print('Что Вам нравится?')
+hobby = input()
+print('Отлично! ',hobby,' - хорошее увлечение.')
