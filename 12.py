262506c6dc05c46fbdb34d5b3fbf7314c3435dbe
bKamilla <k.batrakova@g.nsu.ru>
2024-02-17 00:28:46 +0700

    12.1

--- 12.py
+++ 12.py
@@ -1,3 +1,11 @@
 # Practicum-1
 # Developer: Batrakova K.
 #
+summa = int(input())
+silverq = 96
+goldenq = 96 / 16
+silverprice = 48
+silversum = 48 * silverq
+goldensum = summa - silversum
+goldenprice = goldensum / goldenq
+print(round(goldenprice))
