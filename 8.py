dcab91e0eace7297a43abacb63514ef14f3f5bd4
bKamilla <k.batrakova@g.nsu.ru>
2024-02-17 00:03:56 +0700

    8

---
+++ 8.py
@@ -0,0 +1,11 @@
+# Practicum-1
+# Developer: Batrakova K.
+#
+print('Login: ')
+login = input()
+print('Password: ')
+password = input()
+print('New password: ')
+newpassword = input()
+print('User ',login,' has changed the password to ',newpassword)
+
