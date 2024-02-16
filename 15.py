b520592703b2a730d92731748097a708618498d3
bKamilla <k.batrakova@g.nsu.ru>
2024-02-17 00:43:42 +0700

    15.1

--- 15.py
+++ 15.py
@@ -1,3 +1,12 @@
 # Practicum-1
 # Developer: Batrakova K.
 #
+sm = float(input())
+dumes = sm / 2.54
+futes = dumes / 12
+yard = futes / 3
+miles = yard / 1760
+print(yard,'ярдов')
+print(miles,'мили')
+print(futes,'футов')
+print(dumes,'дюймов')
