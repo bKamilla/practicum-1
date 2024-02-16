855d997caab0a5ac318a70ec63978ae9f2ac4e3c
bKamilla <k.batrakova@g.nsu.ru>
2024-02-17 00:19:42 +0700

    10.1

--- 10.py
+++ 10.py
@@ -1,3 +1,10 @@
 # Practicum-1
 # Developer: Batrakova K.
 #
+number = input()
+nameru = input()
+nameeng = input()
+destru = input()
+desteng = input()
+print('Заканчивается посадка на рейс',number,'авиакомпании',`nameru,'до',destru)
+print('This is the final boarding call for',nameeng,'flight',number,'to',desteng)
