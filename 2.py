fd1440b8d3b78b370e90c3ab326dd43bd141b2d0
bKamilla <k.batrakova@g.nsu.ru>
2024-02-16 23:47:21 +0700

    2.1

--- 2.py
+++ 2.py
@@ -1,4 +1,8 @@
 # Practicum-1
 # Developer: Batrakova K.
 #
-print('Привет, Python!','Hello, Python!','Bonjour Python!','Hej, Python!','Hola, Python!', sep='\n')
+print('''Привет, Python!
+Hello, Python!
+Bonjour Python!
+Hej, Python!
+Hola, Python!''')
